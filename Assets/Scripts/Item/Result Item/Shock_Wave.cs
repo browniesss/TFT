f2364efc3dea1f0f697a1ac74cef1602a795f84c
browniess <abcdkl45678@gmail.com>
2022-03-29 21:58:14 +0900

    d

--- Assets/Scripts/Item/Result Item/Shock_Wave.cs
+++ Assets/Scripts/Item/Result Item/Shock_Wave.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class Shock_Wave : MonoBehaviour
 {
+    List<GameObject> champ_List = new List<GameObject>();
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Enemy")
+        if (other.gameObject.tag == "Enemy")
         {
-            other.GetComponent<ChampionData>().Damaged(100, false);
+            if (!champ_List.Contains(other.gameObject))
+            {
+                other.GetComponent<ChampionData>().Damaged(100, false);
+                champ_List.Add(other.gameObject);
+            }
         }
     }
 
     void Start()
     {
-        
+        champ_List.Clear();
+    }
+
+    public void Shock_Wave_Init()
+    {
+        champ_List.Clear();
     }
 
     void Update()
     {
-        
+
     }
 }
